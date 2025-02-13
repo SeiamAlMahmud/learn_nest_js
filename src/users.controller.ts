06dcaa32f47bc8c3a0bf4bf9b96baeefd5ad1456
SeiamAlMahmud <samahmud49@gmail.com>
2025-02-14 02:57:01 +0600

    user interface for better performance

--- src/users.controller.ts
+++ src/users.controller.ts
@@ -1,12 +1,16 @@
 import { Controller, Get, Req, Res, Param } from '@nestjs/common';
 import { Request, Response } from 'express';
 
+interface VideoParams {
+  id: number;
+  name: string; // if i give number instead of string , i didn't get any error , because in runtime javascript can't check
+}
 @Controller('/users')
 export class UsersController {
-  @Get('/videos/:id')
-  getVideos(@Param('id') param: number) {
+  @Get('/videos/:id/:name')
+  getVideos(@Param() params: VideoParams) {
     //directly use it
-    console.log(param);
-    return `Success ${param}`;
+    console.log(params);
+    return `Success ${params.id}`;
   }
 }

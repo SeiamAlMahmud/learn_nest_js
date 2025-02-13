701c5714081df35572b05f9c3d89d4f702c17b82
SeiamAlMahmud <samahmud49@gmail.com>
2025-02-14 04:37:40 +0600

    basic Crud operation

---
+++ src/dto/create-user.dt.ts
@@ -0,0 +1,5 @@
+export class CreateUserDTO {
+  id: string;
+  name: string;
+  age: number;
+}
--- src/users.controller.ts
+++ src/users.controller.ts
@@ -1,21 +1,50 @@
 import {
-  Controller,
-  Get,
-  Req,
-  Res,
-  Param,
-  Query,
-  Headers,
-  Post,
   Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
 } from '@nestjs/common';
 import { Request, Response } from 'express'; // for data types
-
+import { CreateUserDTO } from './dto/create-user.dt';
+const USERS: CreateUserDTO[] = [];
 @Controller('/users')
 export class UsersController {
-  @Post('/videos')
-  addVideos(@Body() requestData: Record<string, any>) {
-    console.log(requestData);
-    return { success: true };
+  @Post()
+  addUser(@Body() createUserDTO: CreateUserDTO) {
+    USERS.push(createUserDTO);
+    return 'User added successfully';
+  }
+
+  @Get()
+  getUsers() {
+    return USERS;
+  }
+  @Get(':id')
+  getUser(@Param() param: { id: string }) {
+    return USERS.find((user) => user.id === param.id);
+  }
+  @Put(':id')
+  PutUser(
+    @Param() param: { id: string },
+    @Body() updatedUserDTO: CreateUserDTO,
+  ) {
+    const userIdx = USERS.findIndex((user) => user.id === param.id);
+    if (!userIdx) {
+      return;
+    }
+    USERS[userIdx] = updatedUserDTO;
+    return USERS;
+  }
+  @Delete(':id')
+  DeleteUser(@Param() param: { id: string }) {
+    const userIdx = USERS.findIndex((user) => user.id === param.id);
+    if (userIdx !== -1) {
+      USERS.splice(userIdx, 1);
+      return USERS;
+    }
+    return USERS;
   }
 }

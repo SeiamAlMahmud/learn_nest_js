77853435250e350f6f416224544e1b81eedf445a
SeiamAlMahmud <samahmud49@gmail.com>
2025-02-14 02:10:36 +0600

    conditionally redirect

--- src/users.controller.ts
+++ src/users.controller.ts
@@ -1,12 +1,25 @@
-import { Controller, Get, Request as Req } from '@nestjs/common';
-import { Request } from 'express';
+import { Controller, Get, Req, Res } from '@nestjs/common';
+import { Request, Response } from 'express';
+
 @Controller('/users')
 export class UsersController {
-  @Get('/profile/:id')
-  async getProfile(@Req() req: Request) {
-    console.log(req.params.id);
-    return new Promise((resolve) => {
-      resolve({ userId: 1, username: 'john_doe' });
-    });
+  @Get('/profile')
+  getProfile(@Req() req: Request, @Res() res: Response) {
+    const rn = ~~(Math.random() * 10 + 1);
+    if (rn < 5) {
+      return res.redirect('/users/account'); // ✅ Redirect to account
+    } else {
+      return res.redirect('/users/wallet'); // ✅ Redirect to wallet
+    }
+  }
+
+  @Get('/account')
+  redirectRouter() {
+    return 'working account';
+  }
+
+  @Get('/wallet')
+  redirectWallet() {
+    return 'working wallet';
   }
 }

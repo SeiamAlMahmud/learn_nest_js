1ac254636c926b9ccf98c3f9af0df6b4f3ec896e
SeiamAlMahmud <samahmud49@gmail.com>
2025-02-14 01:36:31 +0600

    Http code or Http ststus string provide

--- src/albums.controller.ts
+++ src/albums.controller.ts
@@ -1,8 +1,15 @@
-import { Controller, Get, Request as Req } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Request as Req,
+} from '@nestjs/common';
 import { Request } from 'express';
 @Controller('/albums')
 export class AlbumsController {
   @Get()
+  @HttpCode(HttpStatus.OK)
   getProfile(@Req() req: Request) {
     console.log(req.body);
     return { userId: 1, username: 'Seiam' };

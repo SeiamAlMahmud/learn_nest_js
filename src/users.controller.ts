18b419536c761ac9103cc644a86687e5fd1866b4
SeiamAlMahmud <samahmud49@gmail.com>
2025-02-14 03:17:11 +0600

    get data from body by using the @Body method

--- src/users.controller.ts
+++ src/users.controller.ts
@@ -1,16 +1,21 @@
-import { Controller, Get, Req, Res, Param, Query,Headers } from '@nestjs/common';
-import { Request, Response } from 'express';
+import {
+  Controller,
+  Get,
+  Req,
+  Res,
+  Param,
+  Query,
+  Headers,
+  Post,
+  Body,
+} from '@nestjs/common';
+import { Request, Response } from 'express'; // for data types
 
-interface QueryParams {
-  id: number;
-  name: string; // if i give number instead of string , i didn't get any error , because in runtime javascript can't check
-}
 @Controller('/users')
 export class UsersController {
-  @Get('/videos')
-  getVideos(@Headers() headers: Record<string, any>) {
-    //directly use it
-    console.log(headers);
-    return headers;
+  @Post('/videos')
+  addVideos(@Body() requestData: Record<string, any>) {
+    console.log(requestData);
+    return { success: true };
   }
 }

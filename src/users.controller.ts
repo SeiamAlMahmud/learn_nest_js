d8b9b1368b4e14bffff6bcfc43426cb38d3cb105
SeiamAlMahmud <samahmud49@gmail.com>
2025-02-14 03:01:13 +0600

    Query

--- src/users.controller.ts
+++ src/users.controller.ts
@@ -1,16 +1,16 @@
-import { Controller, Get, Req, Res, Param } from '@nestjs/common';
+import { Controller, Get, Req, Res, Param, Query } from '@nestjs/common';
 import { Request, Response } from 'express';
 
-interface VideoParams {
+interface QueryParams {
   id: number;
   name: string; // if i give number instead of string , i didn't get any error , because in runtime javascript can't check
 }
 @Controller('/users')
 export class UsersController {
-  @Get('/videos/:id/:name')
-  getVideos(@Param() params: VideoParams) {
+  @Get('/videos')
+  getVideos(@Query() query: QueryParams) {
     //directly use it
-    console.log(params);
-    return `Success ${params.id}`;
+    console.log(query.name);
+    return `Success`;
   }
 }

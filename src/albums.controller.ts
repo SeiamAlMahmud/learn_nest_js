33a94aeb1d41502ca505665dbc5e97b7060dcd1d
SeiamAlMahmud <samahmud49@gmail.com>
2025-02-14 01:44:33 +0600

    passthrough use korle Nest Js autoomatically handle kore statuscode

--- src/albums.controller.ts
+++ src/albums.controller.ts
@@ -11,9 +11,10 @@ import { Request, Response } from 'express';
 export class AlbumsController {
   @Get()
   // @HttpCode(HttpStatus.OK)
-  getProfile(@Req() req: Request, @Res() res: Response) {
+  getProfile(@Req() req: Request, @Res({ passthrough: true }) res: Response) {
+    //  passthrough use korle Nest Js autoomatically handle kore statuscode
     console.log(req.body);
-    res.status(200); // HttpStatus.OK);
+    res.status(HttpStatus.OK); // HttpStatus.OK);
     res.json({ userId: 1, username: 'Seiam' });
   }
 }

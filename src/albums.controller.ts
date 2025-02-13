3479f159f070cdce7bdce721e6412cf6e57d3ae2
SeiamAlMahmud <samahmud49@gmail.com>
2025-02-14 01:50:20 +0600

    Set Header in API call and also set multiple Headers in API call

--- src/albums.controller.ts
+++ src/albums.controller.ts
@@ -5,16 +5,18 @@ import {
   HttpStatus,
   Request as Req,
   Res,
+  Header,
 } from '@nestjs/common';
 import { Request, Response } from 'express';
 @Controller('/albums')
 export class AlbumsController {
   @Get()
-  // @HttpCode(HttpStatus.OK)
-  getProfile(@Req() req: Request, @Res({ passthrough: true }) res: Response) {
-    //  passthrough use korle Nest Js autoomatically handle kore statuscode
+  @HttpCode(HttpStatus.OK)
+  @Header('Cache-Control', 'none')
+  @Header('X-name', 'Mahmud')
+  getProfile(@Req() req: Request, @Res() res: Response) {
     console.log(req.body);
-    res.status(HttpStatus.OK); // HttpStatus.OK);
+
     res.json({ userId: 1, username: 'Seiam' });
   }
 }

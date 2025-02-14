a6d2428401557525c2809e70e558c60d67b2e48a
SeiamAlMahmud <samahmud49@gmail.com>
2025-02-14 20:37:02 +0600

    Subdomain routing

--- src/users.controller.ts
+++ src/users.controller.ts
@@ -3,48 +3,20 @@ import {
   Controller,
   Delete,
   Get,
+  HostParam,
+  Ip,
   Param,
   Post,
   Put,
 } from '@nestjs/common';
 import { Request, Response } from 'express'; // for data types
-import { CreateUserDTO } from './dto/create-user.dt';
-const USERS: CreateUserDTO[] = [];
-@Controller('/users')
+
+@Controller({ path: '/users', host: 'app.localhost' }) // external cors check 'app.localhost'
 export class UsersController {
   @Post()
-  addUser(@Body() createUserDTO: CreateUserDTO) {
-    USERS.push(createUserDTO);
+  addUser(@Ip() ip: string, @HostParam() params: Record<string, any>) {
+    console.log(ip);
+    console.log(params.username);
     return 'User added successfully';
-  }
-
-  @Get()
-  getUsers() {
-    return USERS;
-  }
-  @Get(':id')
-  getUser(@Param() param: { id: string }) {
-    return USERS.find((user) => user.id === param.id);
-  }
-  @Put(':id')
-  PutUser(
-    @Param() param: { id: string },
-    @Body() updatedUserDTO: CreateUserDTO,
-  ) {
-    const userIdx = USERS.findIndex((user) => user.id === param.id);
-    if (!userIdx) {
-      return;
-    }
-    USERS[userIdx] = updatedUserDTO;
-    return USERS;
-  }
-  @Delete(':id')
-  DeleteUser(@Param() param: { id: string }) {
-    const userIdx = USERS.findIndex((user) => user.id === param.id);
-    if (userIdx !== -1) {
-      USERS.splice(userIdx, 1);
-      return USERS;
-    }
-    return USERS;
   }
 }
